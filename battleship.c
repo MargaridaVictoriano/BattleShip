02fed0c282a16472fabf5547952d89a4263e2b9c
Margarida Victoriano <38563697+MargaridaVictoriano@users.noreply.github.com>
2020-02-20 12:41:11 +0000

    Update battleship.c

--- battleship.c
+++ battleship.c
@@ -3,9 +3,7 @@
 #define N_MATRIX 10 // tamanho da matriz
 #define N_BOATS 5	// numero de barcos
 
-struct boats{
-	int* boat_size;
-};
+int const boat_size[] = {2,3,4,4,5};
 
 typedef struct map{
 	int matrix[N_MATRIX][N_MATRIX]; 
@@ -23,14 +21,7 @@ MAP* build_matrix(){
 }
 
 
-
-void build_boats(){
-	int* temp = malloc(sizeof(int)*N_BOATS); // criar array para numero de barcos
-	boats -> boat_size = temp;
-}
-
 int main(int argc, char** argv){
-	MAP* player1 = (MAP*)build_matrix();
-
+	MAP* player1 = (MAP*)build_matrix(); //construir mapa do jogador
+	
 }
-
