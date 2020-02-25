b0566ef473d66268393e76845f053a14832607aa
eduardo010174 <50454358+eduardo010174@users.noreply.github.com>
2020-02-25 14:17:05 +0000

    Add files via upload

--- map.h
+++ map.h
@@ -10,54 +10,51 @@ typedef struct map{
 	Battleship 	b	  		4
 	Cruiser		r  		3
 	Submarine	s 			3
-	Destroyer   d 			2
-*/ 
+	Destroyer   d 			2*/
 
+int len_boat(char c){
+   switch(c){
+      case 'c': return 5;
+      case 'b': return 4;
+      case 'r': return 3;
+      case 's': return 3;
+      case 'd': return 2;
+      default: return -1;
+   }
+}
 
-// falta definir relaçao com as restantes funçoes. Temos que tomar uma decisao.
-typedef enum {
-	CARRIER_S    = 5, 
-	BATTLESHIP_S = 4,
-	CRUISER_S    = 3, 
-	SUBMARINE_S  = 3,
-	DESTROYER_S  = 2
-} ShipeSize;
-//Struct para embarcações
-typedef struct ship {
-	char symbol;
-	unsigned int length;
-	char *name;
-} SHIP;
 //Struct para as coordenadas
 typedef struct {
 	int row; 	//linha
 	int column; //colunas
 } COORDINATES;
+
+//Localização do barco
+typedef struct {
+    COORDINATES position;
+    int direction;
+} BOATPOSITION;
+
+//Struct para embarcações
+typedef struct {
+	char id;
+	unsigned int length;
+	char *name;
+	int *damage;
+	BOATPOSITION position;
+} SHIP;
+
+BOATPOSITION placeShip(COORDINATES position, int direction){
+	BOATPOSITION new;
+	new.position = position;
+	new.direction = direction;
+	return new;
+}
+
 //Struct que define cada célula 1*1 (estou a tentar uma coisa)
 /*typedef struct {
 	//char symbol ????
 	COORDINATES position;
 } CELLS;
 */
-//Localização do barco
-typedef struct {
-    char id;
-    int row;
-    int column;
-    int direction;
-} BOATPOSITION;
-
-
-
-BOATPOSITION placeShip(int row, int column, char id, int direction){
-	BOATPOSITION new;
-	new.row = row;
-	new.column = column;
-	new.direction = direction;
-	new.id = id;
-	return new;
-}
-
-
-
 
