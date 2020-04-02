b24ca36962d2195cfd4ea3d2ba567ec078233ac5
eduardo010174 <50454358+eduardo010174@users.noreply.github.com>
2020-04-02 10:59:22 +0100

    Add files via upload

--- version2/player.h
+++ version2/player.h
@@ -1,19 +1,16 @@
 typedef struct {
 	Map *mapPlayer;
-	Map *mapOpponent;
 } Player;
 
 //Construtor do jogador
 Player *buildPlayer(Map *map) {
 	Player* new = (Player*)malloc(sizeof(Player)); 
-	new -> mapPlayer = map;
-	new -> mapOpponent = (Map *)buildMap(); 
+	new -> mapPlayer = map; 
 	return new;
 }
 
 void destroyPlayer(Player* player) {
 	destroyMap(player -> mapPlayer);
-	destroyMap(player -> mapOpponent);
 	free(player);
 }
 
