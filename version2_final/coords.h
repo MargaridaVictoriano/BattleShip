1f1d09c4b51aa1362872ac064e40991ec730597c
eduardo010174 <50454358+eduardo010174@users.noreply.github.com>
2020-04-23 19:01:48 +0100

    Delete coords.h

--- version2_final/coords.h
+++
@@ -1,9 +0,0 @@
-//Struct para as coordenadas
-typedef struct {
-	int row; 	  //linha
-	int column;   //colunas
-	int rotation; //rotacao
-} Coords;
-
-Coords* buildCoords(int, int,int);
-void destroyCoords(Coords*);
